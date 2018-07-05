9f154dfbf5efe804a29bb39295cd2c69d6db8056
hColleen <hcolleen@gmail.com>
2018-07-05 04:24:06 -0700

    start background image work

--- youtube.js
+++ youtube.js
@@ -64,4 +64,36 @@ $(document).ready(function () {
     });
 
 
+	//APOD background request
+	var bgImage;
+	var apodApiUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+	$.ajax({
+		type:"GET",
+		url: apodApiUrl,
+		async: false,
+		dataType: 'json',
+		success: function(data){
+			bgImage = data.url;
+		}
+	});
+	
+	//background image change
+	const root = document.documentElement;
+	const imageBtns = document.querySelectorAll('button');
+	
+	imageBtns.forEach((btn) => {
+		btn.addEventListener('click', handleImgUpdate);
+	});
+	
+	function handleImgUpdate(e){
+		switch(e.target.value) {
+			case 'default':
+			root.style.setProperty('--bg-pic', 'url(PIA17005.jpg)');
+			break;
+			case 'apod':
+			root.style.setProperty('--bg-pic', 'url(bgImage)');
+			break;
+		}
+	}
+	
 });
